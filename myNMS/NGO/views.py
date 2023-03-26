a46f59f6babd3a6dcde9af0f3bc619ffe417b453
ishanray1234 <97338953+ishanray1234@users.noreply.github.com>
2023-03-27 03:19:29 +0530

    4

--- myNMS/NGO/views.py
+++ myNMS/NGO/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
-from django.contrib import messages
+from django.contrib import messages,auth
 from django.contrib.auth.models import User
 from .forms import studentform,pledgeform
 from django.http import HttpResponseRedirect
