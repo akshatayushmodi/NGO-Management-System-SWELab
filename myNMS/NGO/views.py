44df1fe5245fa1d8b37b9fc2128229a195aa0f1b
Akshat Ayush Modi <120508845+akshatayushmodi@users.noreply.github.com>
2023-03-27 01:03:28 +0530

    added few functions

--- myNMS/NGO/views.py
+++ myNMS/NGO/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import studentform,pledgeform
+from django.http import HttpResponseRedirect
+from .models import pledge
 
 # Create your views here.
 def home(request):
@@ -17,7 +21,7 @@ def adminlogin(request):
                 return render(request,'adminpage.html',{})
                 messages.success(request,"successfully logged in")
             else:
-                return redirect('/adminlogin')
+                return redirect('adminlogin')
     else:
         return render(request,'login_admin.html',{})
 def donorlogin(request):
@@ -33,3 +37,39 @@ def donorlogin(request):
             return redirect('/donorlogin')
     else:
         return render(request,'login_donor.html',{})
+def donorview(request):
+    donors_list=User.objects.all()
+    return render(request,'donorsview.html',{'donor_list':donors_list})
+def addstu(request):
+    submitted=False
+    form=studentform
+    if request.method == "POST":
+        form=studentform(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/addstu?submittted=True')
+        else:
+            form=studentform
+            if 'submitted' in request.GET:
+                submitted=True
+    
+    return render(request,'addstudent.html',{'form':form})
+
+def aple(request):
+    submitted=False
+    if request.method == "POST":
+        form=pledgeform(request.POST)
+        if form.is_valid():
+            pledgeobj=pledge#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            pledgeobj.money=form['money']
+            pledgeobj.books=form['books']
+            pledgeobj.uniform=form['uniform']
+            pledgeobj.donor=request.user
+            pledgeobj.status=False
+            pledgeobj.save()
+            return HttpResponseRedirect('/aple?submittted=True')
+    else:
+        form=pledgeform
+        if 'submitted' in request.GET:
+            submitted=True
+    return render(request,'addpledge.html',{'form':form})
